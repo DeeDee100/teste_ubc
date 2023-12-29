0363dd57779bb0879b864f78f8857a0cebb36308
Amanda~Dee <amanda_tinoco00@hotmail.com>
2023-12-29 12:06:49 -0300

    add solr ping

---
+++ README.md
@@ -0,0 +1,3 @@
+link do teste: https://github.com/joaomarcelo81/DesafioUBC
+
+docker run -d -p 8983:8983 --name solr_instance -t solr
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from unidecode  import unidecode
+import pysolr
+import json
 
 def main():
     df = pd.read_csv('aluno.csv')
@@ -8,5 +10,26 @@ def main():
     df.columns=[unidecode(col) for col in df]
     # breakpoint()
     print('oi')
-    
-main()
+
+# Create a client instance. The timeout and authentication options are not required.
+solr = pysolr.Solr('http://localhost:8983/solr/', always_commit=True)
+
+# Note that auto_commit defaults to False for performance. You can set
+# `auto_commit=True` to have commands always update the index immediately, make
+# an update call with `commit=True`, or use Solr's `autoCommit` / `commitWithin`
+# to have your data be committed following a particular policy.
+
+solr.add([
+    {
+        "id": "doc_1",
+        "title": "A test document",
+    },
+    {
+        "id": "doc_2",
+        "title": "The Banana: Tasty or Dangerous?",
+        "_doc": [
+            { "id": "child_doc_1", "title": "peel" },
+            { "id": "child_doc_2", "title": "seed" },
+        ]
+    },
+])
