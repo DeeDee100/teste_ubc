4c89b5e0f23b07a20d41afc931da98ad4618eefc
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2023-12-29 17:04:57 -0300

    sla lek

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 link do teste: https://github.com/joaomarcelo81/DesafioUBC
 
 docker run -d -p 8983:8983 --name solr_instance -t solr
+
+docker exec -it solr_instance solr create_core -c gettingstarted
+
+http://localhost:8983/solr/admin/cores?action=CREATE&name=gettingstarted&instanceDir=gettingstarted
+
+create core -> requests.post('http://localhost:8983/solr/admin/cores?action=CREATE&name=gettingstarted&instanceDir=gettingstarted')
+<!-- tambem funciona com requests.get -->
--- main.py
+++ main.py
@@ -1,55 +1,33 @@
 import pandas as pd
 from unidecode  import unidecode
+import json
 import pysolr
+import requests
 
-# def main():
-#     df = pd.read_csv('aluno.csv')
-#     df.columns=[col.lower() for col in df]
-#     df.columns=[col.replace(" ", "_") for col in df]
-#     df.columns=[unidecode(col) for col in df]
-#     # breakpoint()
-#     print('oi')
-    
-# main()
+pysolr.SolrCoreAdmin()
 
 
+def main():
+    df = pd.read_csv('aluno.csv')
+    df.columns=[col.lower() for col in df]
+    df.columns=[col.replace(" ", "_") for col in df]
+    df.columns=[unidecode(col) for col in df]
+    # breakpoint()
+    print('oi')
 
+core_name = 'aluno'
+
+# create core    
+requests.post(f'http://localhost:8983/solr/admin/cores?action=CREATE&name={core_name}&instanceDir={core_name}')
+
+breakpoint()
 
 # Create a client instance. The timeout and authentication options are not required.
-# solr = pysolr.Solr('http://localhost:8983/solr/', always_commit=True)
+solr = pysolr.Solr(f'http://localhost:8983/solr/{core_name}')
 
-# Note that auto_commit defaults to False for performance. You can set
-# `auto_commit=True` to have commands always update the index immediately, make
-# an update call with `commit=True`, or use Solr's `autoCommit` / `commitWithin`
-# to have your data be committed following a particular policy.
-
-# solr.add([
-#     {
-#         "id": "doc_1",
-#         "title": "A test document",
-#     },
-#     {
-#         "id": "doc_2",
-#         "title": "The Banana: Tasty or Dangerous?",
-#         "_doc": [
-#             { "id": "child_doc_1", "title": "peel" },
-#             { "id": "child_doc_2", "title": "seed" },
-#         ]
-#     },
-# ])
-
-
-
-import json
-
-solr_url = 'http://localhost:8983/solr/teste'
-solr = pysolr.Solr(solr_url)
 
 res = solr.ping()
 response = json.loads(res)
-if response.get('status') == 'OK':
-    print('Connection to Solr server successful!')
-else:
-    print('Connection to Solr server failed.')
-    print(response)
-
+if response.get('status') != 'OK':
+    print(res)
+    raise ConnectionError('could not ping the core. please check log above')
