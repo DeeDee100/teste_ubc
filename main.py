55a2193b53c7057fc4705e1dea782d150fb7b05a
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2024-01-02 13:48:36 -0300

    feat: le todos csv da pasta

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from unidecode import unidecode
 import json
@@ -6,15 +7,14 @@ from dateutil.relativedelta import relativedelta
 import numpy as np
 import subprocess
 
-
 def f(dob):
     r = relativedelta(pd.to_datetime("now"), dob)
 
     return r.years
 
 
-def format_df():
-    df = pd.read_csv("aluno.csv")
+def get_df_json(csv_name):
+    df = pd.read_csv(csv_name)
     df.columns = [col.lower() for col in df]
     df.columns = [col.replace(" ", "_") for col in df]
     df.columns = [unidecode(col) for col in df]
@@ -25,17 +25,29 @@ def format_df():
     json_object = json.loads(df.to_json(orient="records"))
     return json_object
 
+def get_all_csv():
+    path = "."
+    names_list = []
+
+    for filename in os.listdir(path):
+        if filename.endswith('.csv'):
+            names_list.append(filename)
+
+    return names_list
 
 def main():
-    json_to_upload = format_df()
-    core_name = "aluno2"
-    docker_run = f"docker exec -it solr_instance solr create_core -c {core_name}"
-    subprocess.call(docker_run, shell=True)
+    csv_list = get_all_csv()
+    for csv in csv_list:
+        json_to_upload = get_df_json(csv)
+        core_name = "aluno"
+        docker_run = f"docker exec -it solr_instance solr create_core -c {core_name}"
+        subprocess.call(docker_run, shell=True)
 
-    solr = pysolr.Solr(f"http://localhost:8983/solr/{core_name}")
-    solr.add(json_to_upload)
-    solr.commit()
+        solr = pysolr.Solr(f"http://localhost:8983/solr/{core_name}")
+        solr.add(json_to_upload)
+        solr.commit()
 
 
 if __name__ == "__main__":
     main()
+    # get_all_csv()
